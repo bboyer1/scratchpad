20a3a6dc6aa20b057303772d50e92de9a0686bd0
bboye003 <bboye003@plattsburgh.edu>
2021-04-02 23:13:12 -0400

    Added 27 fonts into script

--- text2img.py
+++ text2img.py
@@ -2,16 +2,53 @@
 
 from PIL import Image, ImageDraw, ImageFont
 from string import ascii_letters
-
-
-def build_img(letter, counter):
-    img = Image.new('RGB', (100, 100), color='white')
-    ImageDraw.Draw(img).text((50, 50), letter, fill=(0, 0, 0))
-    img.save(f'images/{letter}_{counter}.jpg')
+from time import sleep
 
 ALPHA = ascii_letters + " " # "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
 
-for counter, letter in enumerate(ALPHA):
-    build_img(letter, counter)
 
-print(f'Image creation complete check the images folder')
+
+font_list = [
+    "Copperplate",
+    "Helvetica",
+    "Arial",
+    "Avenir",
+    "Chalkboard",
+    "Chalkduster",
+    "Charter",
+    "Courier",
+    "Didot",
+    "Futura",
+    "Geneva",
+    "Georgia",
+    "Luminari",
+    "Menlo",
+    "Monaco",
+    "Noteworthy",
+    "Impact",
+    "Optima",
+    "Palatino",
+    "Papyrus",
+    "Phosphate",
+    "Skia",
+    "Tahoma",
+    "Times",
+    "Times New Roman",
+    "Verdana",
+    "Zapfino"
+]
+
+
+def build_img(letter, counter, fonttype, fontsize=12):
+    font = ImageFont.truetype(fonttype, fontsize)
+    img = Image.new('RGB', (100, 100), color='white')
+    ImageDraw.Draw(img).text((50, 50), letter, fill=(0, 0, 0), font=font)
+    img.save(f'images/{letter}_{counter}_{fonttype}.jpg')
+
+for font in font_list:
+    sleep(0.5)
+    for counter, letter in enumerate(ALPHA):
+        build_img(letter, counter, font)
+
+
+print(f'Image creation complete for {len(font_list)} fonts')
