40d24d604bd7e06febf6c1cffe7e20f2d067f5b6
bboye003 <bboye003@plattsburgh.edu>
2021-04-25 21:37:32 -0400

    Cleaning up files, adding updated scripts

---
+++ moat.py
@@ -0,0 +1,25 @@
+### File used to run the inference and return output
+### Bret Boyer
+
+import os
+import subprocess
+import sys
+
+
+directory = "training_images" # test_images
+path = f"/Users/Bret/Desktop/text-to-image/src/{directory}"
+list_of_images = os.listdir(path)
+
+num_of_images = len(list_of_images)
+
+correct = 0
+for i in list_of_images:
+    value = i[0]
+    test = subprocess.run(["python3", "inference.py", f"training_images/{i}"], capture_output=True, text=True)
+    # print(test)
+    if value == test.stdout[0]:
+        correct += 1
+    # print(correct)
+
+
+print(f"Sum: {correct}\nPercentage: {(correct/num_of_images)* 100}")
